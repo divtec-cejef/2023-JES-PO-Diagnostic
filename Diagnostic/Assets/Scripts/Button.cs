5ec7f57027f75ff044c39e8298abb2e425032290
Colin Reist <colin.reist@divtec.ch>
2023-03-31 11:02:35 +0200

    Scripts des boutons
    Rajout du scripts et la focntion de changement de scene

--- Diagnostic/Assets/Scripts/Button.cs
+++ Diagnostic/Assets/Scripts/Button.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void ChangerScene(int sceneID)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(sceneID);
     }
 }
