338ffe428f01fb31535f781e5d77352dd5b48c75
Ethan Odiet <94681675+ethantodomokachi@users.noreply.github.com>
2023-03-31 11:50:48 +0200

    update drag and drop
    Code qui capte quand on drag le bonhomme

--- Diagnostic/Assets/Scripts/Draggable.cs
+++ Diagnostic/Assets/Scripts/Draggable.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Draggable : MonoBehaviour
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        Debug.Log("Begin drag");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        Debug.Log("Dragging");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End drag");
     }
 }
