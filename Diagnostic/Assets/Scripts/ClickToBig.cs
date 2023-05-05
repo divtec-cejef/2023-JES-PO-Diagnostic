302aae68c78a72972aeff042daa73e2afbe5c627
Colin Reist <colin.reist@divtec.ch>
2023-05-05 10:48:12 +0200

    Polissage de la loupe
    Nous prenons les objet par la branche et plus le milieu du verre

--- Diagnostic/Assets/Scripts/ClickToBig.cs
+++ Diagnostic/Assets/Scripts/ClickToBig.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class ClickToBig : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public DragController dragController;
+    
+    public void getBigger() {
+        Debug.Log("getBigger");
+        dragController._lastDragged.transform.localScale = new Vector3(0.18f, 0.05f, 0f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void getSmaller() {
+        Debug.Log("getSmaller");
+        dragController._lastDragged.transform.localScale = new Vector3(0.15f, 0.05f, 0f);
     }
 }
--- Diagnostic/Assets/Scripts/DragController.cs
+++ Diagnostic/Assets/Scripts/DragController.cs
@@ -1,56 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class DragController : MonoBehaviour
 {
     private bool _isDragActive = false;
     private Vector2 _screenPosition;
     private Vector3 _worldPosition;
-    private Draggable _lastDragged;
+    public Draggable _lastDragged;
     private Vector3 basePosition;
+    private float X;
+    private float Y;
 
-    void Awake() {
+    void Awake()
+    {
         DragController[] dragControllers = FindObjectsOfType<DragController>();
-        if (dragControllers.Length > 1) {
+        if (dragControllers.Length > 1)
+        {
             Destroy(gameObject);
         }
+
     }
 
     /**
     * Permet de déplacer un objet en le touchant
     */
     void Update()
-    { 
-        if(_isDragActive) {
-            if(Input.GetMouseButtonUp(0) || Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended) 
+    {
+        if (_isDragActive)
+        {
+            if (Input.GetMouseButtonUp(0) || Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)
             {
                 drop();
                 return;
             }
         }
 
-        if (Input.GetMouseButton(0)) {
+        if (Input.GetMouseButton(0))
+        {
             Vector3 mousePosition = Input.mousePosition;
             _screenPosition = new Vector2(mousePosition.x, mousePosition.y);
         }
-        else if (Input.touchCount > 0) {
+        else if (Input.touchCount > 0)
+        {
             _screenPosition = Input.GetTouch(0).position;
-        } else {
+        }
+        else
+        {
             return;
         }
 
         _worldPosition = Camera.main.ScreenToWorldPoint(_screenPosition);
 
-        if(_isDragActive) {
+        if (_isDragActive)
+        {
             drag();
-        } else {
+        }
+        else
+        {
             RaycastHit2D hit = Physics2D.Raycast(_worldPosition, Vector2.zero);
-            if (hit.collider != null) {
+            if (hit.collider != null)
+            {
                 Draggable draggable = hit.collider.GetComponent<Draggable>();
-                if (draggable != null) {
+                if (draggable != null)
+                {
                     _lastDragged = draggable;
+                    X = _lastDragged.transform.localScale.x;
+                    Y = _lastDragged.transform.localScale.y;
                     initDrag();
                 }
             }
@@ -61,14 +77,19 @@ public class DragController : MonoBehaviour
     {
         basePosition = _lastDragged.transform.position;
         _isDragActive = true;
-    }
-    
-    void drag () {
-        _lastDragged.transform.position = new Vector3(_worldPosition.x, _worldPosition.y);
+        _lastDragged.transform.localScale = new Vector3(X * 2f, Y * 2f, 0f);   
     }
 
-    void drop () {
+    void drag()
+    {
+        _lastDragged.transform.position = new Vector3(_worldPosition.x - 1, _worldPosition.y + 1);
+    }
+
+    void drop()
+    {
         _isDragActive = false;
         _lastDragged.transform.position = basePosition;
+        Debug.Log("getSmaller");
+        _lastDragged.transform.localScale = new Vector3(X, Y, 0f);
     }
 }
