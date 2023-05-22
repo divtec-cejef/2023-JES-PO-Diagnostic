bfc29cf4292bb19de041b7af3c2ce7fea35b0abb
Colin Reist <colin.reist@divtec.ch>
2023-05-22 09:01:21 +0200

    Mini-jeu CPU complet

--- Diagnostic/Assets/Scripts/CpuMiniGame.cs
+++ Diagnostic/Assets/Scripts/CpuMiniGame.cs
@@ -1,26 +1,32 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CpuMiniGame : MonoBehaviour
 {
-    public GameObject objectPrefab; // Le prefab du GameObject à instancier
-    public const int NumberOfObjects = 10; // Le nombre d'objets à faire apparaître
-    public GameObject spawnAreaCenter; // Le centre de la zone de spawn
-    public Vector3 spawnAreaSize; // La taille de la zone de spawn
-    public GameObject aActiver;
-    public GameObject aDesactiver;
+    public const int NumberOfObjects = 10; // Le nombre d'objets à instancier
 
-    private void LauchDust()
+    public Vector3 spawnAreaSize; // La taille de la zone de spawn
+    
+    public GameObject objectPrefab; // Le prefab du GameObject à instancier
+    public GameObject spawnAreaCenter; // Le centre de la zone de spawn
+    public GameObject activate; // Le GameObject à activer
+    public GameObject deactivate; // Le GameObject à désactiver
+
+    /**
+     * Instancie NumberOfObjects objets dans la zone de spawn
+     */
+    private void InstantiateDust()
     {
         // Boucle pour instancier les objets
-        for (int i = 0; i < NumberOfObjects; i++)
+        for (var i = 0; i < NumberOfObjects - 1; i++)
         {
             // Calculez une position aléatoire dans la zone de spawn
             var position = spawnAreaCenter.transform.position;
             var localScale = spawnAreaCenter.transform.localScale;
-            Vector3 randomPosition = new Vector3(
+            var randomPosition = new Vector3(
                 Random.Range(position.x - spawnAreaSize.x / 2, localScale.x + spawnAreaSize.x / 2),
                 Random.Range(position.y - spawnAreaSize.y / 2, localScale.y + spawnAreaSize.y / 2),
-                Random.Range(position.z - spawnAreaSize.z / 2, localScale.z + spawnAreaSize.z / 2)
+                Random.Range(0, 0)
             );
 
             // Instanciez le GameObject à la position aléatoire avec une rotation par défaut
@@ -28,10 +34,14 @@ public class CpuMiniGame : MonoBehaviour
         }
     }
     
+    /**
+     * Active le GameObject aActiver et désactive le GameObject aDesactiver
+     * Appelle LauchDust()
+     */
     public void OnClick()
     {
-        aActiver.SetActive(true);
-        aDesactiver.SetActive(false);
-        LauchDust();
+        activate.SetActive(true);
+        deactivate.SetActive(false);
+        InstantiateDust();
     }
 }
