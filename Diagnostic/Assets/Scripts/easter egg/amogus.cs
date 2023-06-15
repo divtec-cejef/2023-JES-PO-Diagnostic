259777bc561eb7476fb921930c83ddde750e9175
Ethan Odiet <94681675+ethantodomokachi@users.noreply.github.com>
2023-06-15 15:07:50 +0200

    ajout d'un easter egg

---
+++ Diagnostic/Assets/Scripts/easter egg/amogus.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class amogus : MonoBehaviour
+{
+    public GameObject img;
+
+    public void clicTable()
+    {
+        img.SetActive(true);
+    }
+}
