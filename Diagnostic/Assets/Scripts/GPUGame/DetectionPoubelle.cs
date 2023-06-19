c7131871d4163147ff1cafc96d2f0b6884d33f16
Colin Reist <colin.reist@divtec.ch>
2023-06-19 13:23:22 +0200

    Finalistation des graphisme et du jeu

--- Diagnostic/Assets/Scripts/GPUGame/DetectionPoubelle.cs
+++ Diagnostic/Assets/Scripts/GPUGame/DetectionPoubelle.cs
@@ -1,41 +1,49 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
+
 
 public class DetectionPoubelle : MonoBehaviour
 {
-    public GameObject poubelleOuverte; //Objet à afficher quand le déchet est jeté dans la poubelle
-    public GameObject poubelleFermee; //Objet à afficher quand le déchet n'est pas jeté dans la poubelle
     private GameObject _hand;
+    private GameObject _infos;
+    public GameObject banane;
+    public GameObject amongus;
+    public GameObject cable;
+    public GameObject duck;
 
     public void Start()
     {
         _hand = GameObject.Find("hand_tuto");
+        _infos = GameObject.Find("infos");
     }
-    
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         DetectionFinNiveau.ObjetsRetires++;
-        poubelleOuverte.SetActive(true);
-        
-        //Cache la poubelle dérrière le décor
-        var transformPosition = poubelleFermee.transform.position;
-        transformPosition.z = -10;
     }
-    
+
     public void OnTriggerExit2D(Collider2D collision)
     {
-        poubelleOuverte.SetActive(false);
-        var transformPosition = poubelleFermee.transform.position;
-        transformPosition.z = 10;
-        
-        GameObject gameObject = collision.gameObject;
-        
-        Destroy(gameObject);
-        
+        GameObject collisionGameObject = collision.gameObject;
+
+        switch (collisionGameObject.tag)
+        {
+            case "Banane":
+                banane.SetActive(true);
+                break;
+            case "amongus":
+                amongus.SetActive(true);
+                break;
+            case "cable":
+                cable.SetActive(true);
+                break;
+            case "duck":
+                duck.SetActive(true);
+                break;
+        }
+
+        Destroy(collisionGameObject);
+
         _hand.SetActive(false);
+        _infos.SetActive(false);
     }
 }
