5683c0dc1e05b8ebe5baa96d0a648239016234c5
Colin Reist <94681686+colin-reist@users.noreply.github.com>
2023-09-25 09:50:14 +0200

    Modification de cable non-importe

--- Diagnostic/Assets/Scripts/CableGame/main.cs
+++ Diagnostic/Assets/Scripts/CableGame/main.cs
@@ -7,9 +7,6 @@ public class main : MonoBehaviour
     static public main instance;
 
     public int numCables;
-    public GameObject Game;
-    public GameObject winText;
-    public GameObject finNiv;
     private int onCount = 0;
 
     private void Awake()
@@ -19,9 +16,14 @@ public class main : MonoBehaviour
 
     private IEnumerator FinDeNiveau()
     {
+<<<<<<< Updated upstream
         yield return new WaitForSeconds(1);
         Game.SetActive(false);
         finNiv.SetActive(true);
+=======
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("8-Tobias-fin");
+>>>>>>> Stashed changes
 
     }
     public void OnCableOn(int points)
@@ -29,7 +31,6 @@ public class main : MonoBehaviour
         onCount = onCount + points;
         if (onCount == numCables)
         {
-            winText.SetActive(true);
             StartCoroutine(FinDeNiveau());
         }
     }
